d36985e7e54876009040bd5155963b3547002d8c
Thiago Augustus de Oliveira <thiagoaugustusdeoliveira@gmail.com>
2015-11-17 17:34:06 +0000

    Added new handlers

--- main.go
+++ main.go
@@ -15,8 +15,31 @@ func main() {
 	// Instantiate a new router
 	r := httprouter.New()
 	
+	r.POST("/user", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		// Stub an user to be populated from the body		
+		u := models.User{}
+		
+		// Populated the user data
+		json.NewDecoder(r.Body).Decode(&u)
+		
+		// Add an Id
+		u.Id = "foo"
+		
+		uj, _ := json.Marshal(u)
+		
+		//Write Content-Type, statuscode, payload
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(201)
+		fmt.Fprintf(w, "%s", uj)
+	})
+	
+	r.DELETE("/user/:id", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) { 
+		// TODO: only write status for now
+		w.WriteHeader(200)
+	})
+	
 	// Add a handler on /test
-	r.GET("/:id", func(w http.ResponseWriter, http *http.Request, p httprouter.Params) {
+	r.GET("/user/:id", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 		// Stub an user example
 		u := models.User{
 			Name:   "Thiago Oliveira",
